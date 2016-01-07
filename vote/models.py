44493d1dae51d25482a0c926b419fd5b6bf48407
sasha361322 <sasha361322@gmail.com>
2016-01-07 23:18:10 +0300

    2
    2

--- vote/models.py
+++ vote/models.py
@@ -5,15 +5,16 @@ from django.db import models
 class Vote(models.Model):
     class Meta:
         db_table = 'vote'
-    question = models.CharField(max_length = 200)
+    question = models.CharField(max_length=200)
     date = models.DateTimeField()
     count = models.IntegerField()
-    is_single = models.BooleanField()
+    is_single = models.BooleanField(default=True)
 
 
 class Answer(models.Model):
     class Meta:
         db_table = 'answer'
-    text = models.CharField(max_length = 200)
-    count = models.IntegerField()
+    text = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
     number = models.IntegerField()
+    answer_vote = models.ForeignKey(Vote, null=True)
